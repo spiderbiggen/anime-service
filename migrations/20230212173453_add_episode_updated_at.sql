021a095e651a9c30ab50586b89a528408d7eb57f
Stefan Breetveld <spiderbiggen@gmail.com>
2023-02-12 20:36:08 +0100

    improve migration

--- migrations/20230212173453_add_episode_updated_at.sql
+++ migrations/20230212173453_add_episode_updated_at.sql
@@ -1,8 +1,12 @@
 ALTER TABLE episode_download
     ADD COLUMN updated_at TIMESTAMPTZ;
 
-UPDATE episode_download
-SET updated_at = created_at;
+UPDATE episode_download ed
+SET updated_at = (SELECT max(edr.created_at)
+                  from episode_download_resolution edr
+                  WHERE edr.episode_download_id = ed.id
+                  group by edr.episode_download_id)
+WHERE true;
 
 ALTER TABLE episode_download
     ALTER COLUMN created_at SET DEFAULT now(),
